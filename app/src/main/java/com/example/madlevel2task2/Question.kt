8eb7d94c0778e8b1940fad82fcb0856bbc9ab70d
KeruFC <kerubela1@gmail.com>
2020-09-19 20:38:13 +0200

    Add questions array

---
+++ app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -0,0 +1,15 @@
+package com.example.madlevel2task2
+
+class Question (
+    var question:String
+){
+    companion object {
+        val QUESTIONS = arrayOf(
+            "The earth is round.",
+            "This app is coded in Python.",
+            "You have friends.",
+            "A 'val' and 'var' are the same.",
+            "Your opinion is subjective."
+        )
+    }
+}
